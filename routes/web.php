e39211df6670507ed1d89f93e75bbb4fb133fa1c
Bilala21 <bilal.kalri@gmail.com>
2022-04-21 09:52:55 +0500

    In this commit i upload all files to github

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\StripePaymentController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('stripe', 'Strim@stripe');
+// Route::post('stripe', 'Strim@stripePost')->name('stripe.post');
+Route::get('stripe',[StripePaymentController::class,'stripe']);
+Route::post('stripe',[StripePaymentController::class,'stripePost'])->name('stripe.post');
